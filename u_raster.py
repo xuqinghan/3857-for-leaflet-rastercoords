cd639cce6af8a5a958787faf921bfce20a8c7732
xuqinghan <xuqinghan@gmail.com>
2020-11-11 20:13:45 +0800

    for js

--- u_raster.py
+++ u_raster.py
@@ -69,67 +69,85 @@ def transform_point(transform, point):
     return [lon, lat]
 
 
-def createProj2ImageRowCol(transform):
+def createProjXY2ImageRowCol(transform):
+    dTemp = transform[1]*transform[5] - transform[2]*transform[4]
+    
+    def xy2RowCol(x, y):
+        dCol = (transform[5]*(x - transform[0]) - 
+            transform[2]*(y - transform[3])) / dTemp + 0.5;
+        dRow = (transform[1]*(y - transform[3]) - 
+            transform[4]*(x - transform[0])) / dTemp + 0.5;
 
-    def lonlat2xy(lon, lat):
-        dTemp = transform[1]*transform[5] - transform[2]*transform[4];
+        return int(dRow), int(dCol) 
 
-        dCol = (transform[5]*(lon - transform[0]) - 
-            transform[2]*(lat - transform[3])) / dTemp + 0.5;
-        dRow = (transform[1]*(lat - transform[3]) - 
-            transform[4]*(lon - transform[0])) / dTemp + 0.5;
+    return xy2RowCol
 
-        return int(dRow), int(dCol)
+def createProjXY2ImageRowCol_js(transform):
+    dTemp = transform[1]*transform[5] - transform[2]*transform[4]
+    return f'''
+const fn_transform_3857_image = (x, y) => {{
+    const dTemp = {dTemp}
+    const dCol = ({transform[5]}*(x - {transform[0]}) - 
+        {transform[2]}*(y - {transform[3]})) / {dTemp} + 0.5;
+    const dRow = ({transform[1]}*(y - {transform[3]}) - 
+        {transform[4]}*(x - {transform[0]})) / {dTemp} + 0.5;
 
-    return lonlat2xy
+    return [dRow, dCol] 
+}}
+
+'''
 
 
 
 
-def createImageRowCol2LonLat(transform):
+def createImageRowCol2ProjXY(transform):
 
-    def RowCol2LonLat(iRow, iCol):
+    def RowCol2ProjXY(iRow, iCol):
 
-        lon = transform[0] + transform[1] * iCol + transform[2] * iRow;
-        lat = transform[3] + transform[4] * iCol + transform[5] * iRow;
-        return lon, lat
+        x = transform[0] + transform[1] * iCol + transform[2] * iRow;
+        y = transform[3] + transform[4] * iCol + transform[5] * iRow;
+        return x, y
 
-    return RowCol2LonLat
+    return RowCol2ProjXY
 
 
 
 if __name__ == '__main__':
-    #原图是3857坐标系  为了充满平面直角
-    fname = 'D:/xqh/4dev/zgy/data-origin/gis/谷歌卫星_200907103109.tif'
+    #fname = 'D:/xqh/4dev/zgy/data-origin/gis/谷歌卫星_200907103109.tif'
+    fname = 'D:/dev/zgy/data-origin/gis/谷歌卫星_200907103109.tif'
     dataset = gdal.Open(fname)
 
     epsg_src = get_EPSG(dataset)
-    print(epsg_src)
+    #print(epsg_src)
 
     cols = dataset.RasterXSize
     rows = dataset.RasterYSize
-    print(cols, rows)
+    #print('cols, rows', cols, rows)
     #d3-geo 可以
     transform_3857_4326 = get_transform_by_epsg(epsg_src, epsg_dst=4326)
     #image
     transform_para_image_3857 = dataset.GetGeoTransform()
-    print(transform_para_image_3857)
+    #print(transform_para_image_3857)
     #print(transform_image_3857)
-    fn_transform_image_3857 = createImageRowCol2LonLat(transform_para_image_3857)
-    fn_transform_3857_image = createProj2ImageRowCol(transform_para_image_3857)
+    fn_transform_image_3857 = createImageRowCol2ProjXY(transform_para_image_3857)
+    fn_transform_3857_image = createProjXY2ImageRowCol(transform_para_image_3857)
 
     #x, y->3857
-    x, y = int(cols/2), int(rows/2)
-    print(x, y)
-    row_col_3857 = fn_transform_image_3857(x, y)
-    print('row_col_3857 ', row_col_3857)
-    lat,lon, z = transform_3857_4326.TransformPoint(*row_col_3857)
+    col, row   = int(cols/2), int(rows/2)
+    print('row col in image = ', row, col)
+    xy_3857 = fn_transform_image_3857(row, col)
+    print('x y in 3857 =', xy_3857)
+    #x 在前
+    lat,lon, z = transform_3857_4326.TransformPoint(*xy_3857)
     # 39.847557957355505 116.18968963622903
     print(lat,lon)
     # 4326->3857
     transform_4326_3857 = get_transform_by_epsg(4326, epsg_src)
-    row_3857,col_3857, _ = transform_4326_3857.TransformPoint(lat,lon)
-    print('row_col_3857 ', row_col_3857)
+    # x在前
+    x_3857,y_3857, _ = transform_4326_3857.TransformPoint(lat,lon)
+    print('x y in 3857 ', x_3857, y_3857)
     #3857 ->x y   
-    x, y = fn_transform_3857_image(row_3857, col_3857)
-    print(x, y)
+    row, col = fn_transform_3857_image(x_3857, y_3857)
+    print('row col in image = ', row, col)
+    fn_transform_3857_image_str_js = createProjXY2ImageRowCol_js(transform_para_image_3857)
+    print(fn_transform_3857_image_str_js)
